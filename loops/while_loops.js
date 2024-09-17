fc1db7e4fe92127a11ec8cd54fdbb543c08b8d61
Jay Sandhu <jai.sandhu2002@gmail.com>
2024-09-17 14:57:23 +0000

    loop

---
+++ loops/while_loops.js
@@ -0,0 +1,24 @@
+//while loop
+/*
+ while (condition) {
+    
+}
+ */
+let i=0
+while (i<=10) {
+    //console.log(`value is ${i}`)
+    i++;
+}
+let myArr=[1,2,3,4,5];
+let j=0;
+while(j<myArr.length){
+   // console.log(`value is ${myArr[j]}`)
+    j++;
+}
+
+//do-while loop //->run atleast once weather the condition is satisfied or not
+let score=1
+do {
+    console.log(`score is ${score}`)
+    score++;
+} while (score<=10);
